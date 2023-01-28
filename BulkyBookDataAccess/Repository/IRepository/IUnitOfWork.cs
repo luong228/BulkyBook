610625b92a297bdeeb7a881b34d6593c24e4d6da
luong228 <90311663+luong228@users.noreply.github.com>
2023-01-28 10:20:37 +0700

    Create Company Model

--- BulkyBookDataAccess/Repository/IRepository/IUnitOfWork.cs
+++ BulkyBookDataAccess/Repository/IRepository/IUnitOfWork.cs
@@ -11,6 +11,7 @@ namespace BulkyBook.DataAccess.Repository.IRepository
         ICategoryRepository CategoryRepository { get; }
         ICoverTypeRepository CoverTypeRepository { get; }
         IProductRepository ProductRepository { get; }
+        ICompanyRepository CompanyRepository { get; }
         void Save();
     }
 }
--- BulkyBookDataAccess/Repository/UnitOfWork.cs
+++ BulkyBookDataAccess/Repository/UnitOfWork.cs
@@ -16,10 +16,12 @@ namespace BulkyBook.DataAccess.Repository
             CategoryRepository = new CategoryRepository(_db);
             CoverTypeRepository = new CoverTypeRepository(_db);
             ProductRepository = new ProductRepository(_db);
+            CompanyRepository= new CompanyRepository(_db);
         }
         public ICategoryRepository CategoryRepository { get; private set; }
         public ICoverTypeRepository CoverTypeRepository { get; private set; }
         public IProductRepository ProductRepository { get; private set; }
+        public ICompanyRepository CompanyRepository { get; private set; }
 
         public void Save()
         {
