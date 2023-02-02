b4994e998fc5b7ef3947ac126b6a1b10883f7ab3
luong228 <90311663+luong228@users.noreply.github.com>
2023-02-02 11:08:46 +0700

    Complete Order Creation

--- BulkyBook.Models/OrderHeader.cs
+++ BulkyBook.Models/OrderHeader.cs
@@ -32,13 +32,15 @@ namespace BulkyBook.Models
 		[Required]
 		public string Name { get; set; }
 		[Required]
-		public string? StreetAddress { get; set; }
+		public string StreetAddress { get; set; }
 		[Required]
-		public string? City { get; set; }
+		public string PhoneNumber { get; set; }
 		[Required]
-		public string? State { get; set; }
+		public string City { get; set; }
 		[Required]
-		public string? PostalCode { get; set; }
+		public string State { get; set; }
+		[Required]
+		public string PostalCode { get; set; }
 
 	}
 }
